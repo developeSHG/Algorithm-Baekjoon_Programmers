f1b093b90727dc4c25f94a23c34982d3b9bb4297
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-15 04:10:47 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,7 +1,7 @@
 function solution(k, score) {
     var answer = [],
         award = [];
-
+    // var award = [];
     for (const iterator of score) {
         if (k > award.length) {
             award.push(iterator);
@@ -9,7 +9,7 @@ function solution(k, score) {
             answer.push(award.at(0));
         } else if (iterator >= award.at(0)) {
             award.push(iterator);
-            award.sort((a, b) => a - b).splice(0, 1);
+            award.sort((a, b) => a - b).shift();
             answer.push(award.at(0));
         } else answer.push(award.at(0));
     }
