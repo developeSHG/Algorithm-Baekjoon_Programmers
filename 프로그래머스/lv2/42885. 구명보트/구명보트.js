cffcf8b72990343a8b08c3076d3242f851431c07
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-17 17:39:19 +0900

    [level 2] Title: 구명보트, Time: 11.42 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -2,10 +2,7 @@ function solution(people, limit) {
     var answer = 0;
 
     people.sort((a, b) => b - a);
-
-    let j = people.length - 1;
-
-    for (let i = 0; i <= j; ++i) {
+    for (let i = 0, j = people.length - 1; i <= j; ++i) {
         if (limit >= people[i] + people[j]) {
             ++answer;
             --j;
