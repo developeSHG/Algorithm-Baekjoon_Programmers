0a1e870eba4d150f6efa4ddd73a681be679c8581
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-20 03:35:46 +0900

    [level 2] Title: 피로도, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,28 +1,21 @@
-function solution(k, dungeons) {
-    const length = dungeons.length;
-    const visited = Array(length).fill(false);
+const solution = (k, dungeons) => {
     let answer = 0;
-    let temp = 0;
+    const visit = Array.from({ length: dungeons.length }, (_) => false);
 
-    const dfs = (currentK, cnt) => {
-        for(let i = 0; i < length; i++) {
-            // 해당 던전을 아직 방문하지 않았고
-            // 던전의 최소 피로도가 현재 피로도보다 작거나 같으면
-            if(!visited[i] && currentK >= dungeons[i][0]) {
-            	// 해당 던전 방문표시
-                visited[i] = true;
-                // 재귀~
-                dfs(currentK - dungeons[i][1], cnt + 1);
-                // 추후 다시 방문해야 하므로 방문 표시 해제
-                visited[i] = false;
+    const dfs = (k, cnt) => {
+        for (const i in dungeons) {
+            const [need, consume] = dungeons[i];
+
+            if (!visit[i] && k >= need) {
+                visit[i] = true;
+                dfs(k - consume, cnt + 1);
+                visit[i] = false;
             }
         }
-        // 최대 던전 수 갱신
         answer = Math.max(answer, cnt);
-        return;
-    }
+    };
 
     dfs(k, 0);
 
     return answer;
-}
+};
