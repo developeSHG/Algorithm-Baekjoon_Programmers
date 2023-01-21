814dc5c67bba10337a9800bba5804a5436c529fa
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-22 06:22:49 +0900

    [unrated] Title: 점 찍기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.js
@@ -0,0 +1,9 @@
+function solution(k, d) {
+    let answer = 0;
+
+    const getY = (num) => Math.sqrt(d ** 2 - num ** 2);
+
+    for (let i = 0; i <= d; i += k) answer += Math.floor(getY(i) / k) + 1;
+
+    return answer;
+}
