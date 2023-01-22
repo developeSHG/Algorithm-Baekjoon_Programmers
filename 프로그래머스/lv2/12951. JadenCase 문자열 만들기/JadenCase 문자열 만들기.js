eb280f268a5b5d9b912d31ee5397e6a24f6603e8
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-22 10:25:21 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,6 +1,1 @@
-function solution(s) {
-    return s
-        .split(" ")
-        .map((v) => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase())
-        .join(" ");
-}
+const solution = (s) => s.split(" ").map((v) => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase()).join(" ");
