ea5695cb678a251a4155d34e9194ee91b39a7686
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-22 10:39:22 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.71 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,8 +1,5 @@
 function solution(A, B) {
     A.sort((a, b) => a - b);
     B.sort((a, b) => b - a);
-    return A.reduce((acc, e, i) => {
-        acc += e * B[i];
-        return acc;
-    }, 0);
+    return A.reduce((acc, e, i) => (acc += e * B[i]), 0);
 }
