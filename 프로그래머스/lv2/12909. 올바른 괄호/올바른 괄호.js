7a5749d775610ba0ac3dc6d31c658b18017d6a13
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-24 06:09:49 +0900

    [level 2] Title: 올바른 괄호, Time: 5.17 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,10 +1,8 @@
-function solution(s){
-    let cum = 0
-    for (let paren of s) {
-        cum += paren === '('? 1: -1
-        if(cum < 0) {
-            return false
-        }
+function solution(s) {
+    let res = 0;
+    for (let e of s) {
+        e === "(" ? ++res : --res;
+        if (res < 0) return false;
     }
-    return cum === 0? true: false;
+    return !res;
 }
