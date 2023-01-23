3266293c7a1d1b96ee742f3536ebfef38de4ad7a
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-24 07:31:12 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4.95 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    let convert = 0,
+        erase = 0;
+
+    while (s !== "1") {
+        erase += s.split("0").length - 1;
+        s = s.replace(/0/g, "");
+        ++convert;
+
+        s = s.length.toString(2);
+        
+    }
+
+    return [convert, erase];
+}
