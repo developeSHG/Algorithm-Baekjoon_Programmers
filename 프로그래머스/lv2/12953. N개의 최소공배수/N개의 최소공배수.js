7761c15d561e35a523bcca1e0bf693c87ea67a3f
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-01-30 22:23:06 +0900

    [level 2] Title: N개의 최소공배수, Time: 15.17 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,13 +1,5 @@
 function solution(arr) {
-    arr.sort((a, b) => b - a);
     let loop = 1;
-    while (
-        arr.reduce((acc, e) => {
-            return (loop * arr[0]) % e === 0 ? ++acc : acc;
-        }, 0) != arr.length
-    ) {
-        ++loop;
-    }
-
+    while (arr.reduce((acc, e) => (!((loop * arr[0]) % e) ? ++acc : acc), 0) !== arr.length) ++loop;
     return loop * arr[0];
 }
