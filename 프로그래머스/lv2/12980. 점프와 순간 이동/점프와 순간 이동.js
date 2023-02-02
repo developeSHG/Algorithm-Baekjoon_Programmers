26a2fc92a15c5a80a4e221fc4bf24da4499a3f42
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-02 18:48:22 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,15 +1,1 @@
-function solution(n) {
-    var acc = 0;
-    if (n == 1)
-        return 1;
-    if (Math.floor(n % 2) === 1)
-        acc++;
-    
-    while (n !== 1) {
-        if (Math.floor(n / 2) % 2 === 1)
-            acc++;
-        n = Math.floor(n / 2);
-    }
-    
-    return acc;
-}
+const solution = (n) => [...n.toString(2)].filter(Number).length;
