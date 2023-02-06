b61d31e7f3910f64f1bea11bb07e3f14b39d06c1
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-07 00:25:05 +0900

    [level 2] Title: 행렬의 곱셈, Time: 3.44 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,7 +1,16 @@
 function solution(arr1, arr2) {
-    return arr1.map((row) => {
-        return arr2[0].map((_, col) => {
-            return row.reduce((acc, e, idx) => acc + e * arr2[idx][col], 0);
-        });
-    });
+    return arr1.reduce((acc, eArr1) => {
+        const temp = [];
+        let cnt = 0;
+        while (cnt < arr2[0].length) {
+            let val = 0;
+            for (let i = 0; i < eArr1.length; ++i)
+                val += eArr1[i] * arr2[i][cnt];
+
+            temp.push(val);
+            ++cnt;
+        }
+        acc.push(temp);
+        return acc;
+    }, []);
 }
