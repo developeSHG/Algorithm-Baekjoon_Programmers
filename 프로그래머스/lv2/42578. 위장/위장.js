519339510602029456ebbeec0defc8bd0240e53d
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-07 13:54:32 +0900

    [level 2] Title: 위장, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -2,8 +2,8 @@ function solution(clothes) {
     var answer = 1;
     const map = new Map();
 
-    clothes.forEach((pair) => map.set(pair[1], (map.get(pair[1]) ? map.get(pair[1]) : 1) + 1));
+    clothes.forEach((pair) => map.set(pair[1], map.get(pair[1]) ? map.get(pair[1]) + 1 : 1));
 
-    for (const [key, value] of map.entries()) answer *= value;
+    for (const [key, value] of map.entries()) answer *= (value + 1); // 아무것도 안입은 경우가 있어서 + 1
     return --answer;
 }
