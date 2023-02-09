3e531dff49d24bbefd69a7d07464c5207d4b54c1
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-09 21:06:16 +0900

    [level 2] Title: 튜플, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/튜플.js
+++ 프로그래머스/lv2/64065. 튜플/튜플.js
@@ -1,4 +1,15 @@
 function solution(s) {
-    var answer = [];
-    return answer;
+    s = s.replace(/{/g, "").replace(/},/g, " ").replace(/}/g, "").split(" ").sort((a, b) => a.length - b.length);
+    return s.reduce((acc, el) => {
+        el = el.split(",");
+        if (acc.length < el.length) {
+            acc.forEach((v) => el = el.filter((o) => o !== v));
+            acc.push(el.join());
+        }
+        else if (acc.length > el.length) {
+            el.forEach((v) => acc = acc.filter((o) => o !== v));
+            acc.unshift(v);
+        }
+        return acc;
+    }, []).map(Number);
 }
