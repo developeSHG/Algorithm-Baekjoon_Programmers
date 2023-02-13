d03059dfebb586a08d8f78499df4982221554f9d
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-13 13:59:26 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,4 +1,4 @@
-function solution(progresses, speeds) {
+const solution = (progresses, speeds) => {
     return progresses.map((el, idx) => Math.ceil((100 - el) / speeds[idx])).reduce((queue, el) => {
         (queue.length < 1) ? queue.push([el]) : (queue[queue.length - 1][0] >= el) ? queue[queue.length - 1].push(el) : queue.push([el]);
         return queue;
