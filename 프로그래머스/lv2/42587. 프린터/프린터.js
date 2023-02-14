ba42aa7e99456d1a0dc93ecdea13207e5b8eba78
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-14 16:34:51 +0900

    [level 2] Title: 프린터, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.js
@@ -0,0 +1,17 @@
+function solution(priorities, location) {
+    const recursive = (res, data) => {
+        if (data.filter((e, i) => data[0] < e && 0 < i).length <= 0) {
+            if (0 == location) return res.length + 1;
+            res.push(data[0]);
+            --location;
+        }
+        else {
+            location = (location - 1 < 0) ? data.length - 1 : location - 1;
+            data.push(data[0]);
+        }
+        data.shift();
+        return recursive(res, data);
+    };
+
+    return recursive([], priorities);
+}
