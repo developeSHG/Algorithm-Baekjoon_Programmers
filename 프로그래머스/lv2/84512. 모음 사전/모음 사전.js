45c46ee4b6263054cfd577aaa630f7ffeb290805
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-23 17:39:04 +0900

    [level 2] Title: 모음 사전, Time: 3.55 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,38 +1,17 @@
 function solution(word) {
+    const res = {};
+    let idx = 0;
     const alphabet = ['A', 'E', 'I', 'O', 'U'];
 
-    const dfs = (cur, cnt, complete) => {
-        cur += alphabet[0];
+    (dfs = (now, cnt) => {
+        if (cnt > 5) return;
+        res[now] = idx++;
+        if (now === word) return;
 
-        if (cur.length > alphabet.length) {
-            cur = cur.slice(0, cur.length - 1).split('');
-
-            for (let i = cur.length - 1; i >= 0; --i) {
-                if (complete > i)
-                    break;
-
-                if (i === complete && cur[complete] === alphabet[alphabet.length - 1]) {
-                    ++complete;
-                    if (i + 1 < cur.length) cur[i + 1] = alphabet[alphabet.length - 1];
-                    break;
-                }
-
-                if ((alphabet.indexOf(cur[i]) + 1) % alphabet.length === 0) {
-                    cur[i] = 'z';
-                }
-                else {
-                    cur[i] = alphabet[(alphabet.indexOf(cur[i]) + 1)];
-                    break;
-                };
-            }
-            cur = cur.filter((e) => e !== 'z').join().replaceAll(',', '');
+        for (let i = 0; i < 5; ++i) {
+            next = now + alphabet[i];
+            dfs(next, cnt + 1);
         }
-
-        if (cur === word)
-            return cnt + 1;
-
-        return dfs(cur, cnt + 1, complete);
-    };
-
-    return dfs('', 0, 0);
+    })('', 0);
+    return res[word];
 }
