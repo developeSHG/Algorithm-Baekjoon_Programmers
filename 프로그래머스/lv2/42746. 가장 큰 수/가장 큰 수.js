f8cead23ffeef4fd442489e64b7d7ee13be0a22c
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-23 22:05:20 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -2,7 +2,7 @@ function solution(numbers) {
     const answer = numbers.sort((a, b) => {
         a += '';
         b += '';
-        return (b + a) - (a + b);
+        return (b + a) * 1 - (a + b) * 1;
     }).join("").toString();
     return answer[0] === '0' ? '0' : answer;
 }
