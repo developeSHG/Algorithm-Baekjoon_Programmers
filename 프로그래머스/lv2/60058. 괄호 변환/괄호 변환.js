1c326f34c53fc5efea6afcc3c490d44e4a096af3
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-27 18:38:32 +0900

    [level 2] Title: 괄호 변환, Time: 1.49 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
@@ -12,10 +12,5 @@ function solution(p) {
         }
     }
 
-    let [upright, check] = [true, 0];
-    for (w of u) {
-        (w === '(') ? ++check : --check;
-        if (!(upright = (check < 0) ? false : true)) break;
-    }
-    return (upright) ? u + solution(v) : `(${solution(v)})${u.slice(1, u.length - 1).replace(/\(|\)/g, a => a === "(" ? ")" : "(")}`;
+    return (u[0] === "(" && u[u.length - 1] == ")") ? u + solution(v) : `(${solution(v)})${u.slice(1, u.length - 1).replace(/\(|\)/g, a => a === "(" ? ")" : "(")}`;
 }
