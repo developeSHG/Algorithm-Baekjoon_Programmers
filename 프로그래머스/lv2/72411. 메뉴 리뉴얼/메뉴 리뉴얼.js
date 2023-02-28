02630652b6cf09cbaaccd5714ec6fb148c87f16b
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-02-28 19:17:36 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 4.13 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -0,0 +1,34 @@
+const combination = (idx, start, data, n, str, obj) => {
+    if (start === n) {
+        const str_tmp = data.join("");
+        obj[str_tmp] = ((obj[str_tmp]) || 0) + 1;
+        return obj;
+    }
+
+    for (let i = idx; i < str.length; ++i) {
+        data[start] = str[i];
+        obj = combination(i + 1, start + 1, data, n, str, obj);
+    }
+    return obj;
+}
+
+function solution(orders, course) {
+    orders = orders.map(el => [...el].sort().join(""));
+    return course.reduce((acc, n) => {
+        let [pivot, arr] = [0, []];
+        const result = orders.reduce((obj, str) => combination(0, 0, [], n, str, obj), {});
+
+        for (key in result) {
+            if (result[key] < 2) continue;
+
+            if (result[key] > pivot) {
+                pivot = result[key];
+                arr.length = 0;
+                arr.push(key);
+            }
+            else if (result[key] === pivot)
+                arr.push(key);
+        }
+        return acc.concat(arr);
+    }, []).sort();
+}
