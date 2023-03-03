475c3ef66b29ae2e2feb9fb8f7d5e0caff789667
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-03 11:59:25 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 6.53 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/lv2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -5,8 +5,9 @@ function solution(files) {
             TAIL = file.slice(file.indexOf(NUMBER) + NUMBER.length, file.length);
         return { HEAD: HEAD, NUMBER, NUMBER, TAIL: TAIL };
     }).sort((a, b) => {
-        if (a.HEAD.toLowerCase() > b.HEAD.toLowerCase()) return 1;
-        else if (a.HEAD.toLowerCase() === b.HEAD.toLowerCase()) return (a.NUMBER * 1) - (b.NUMBER * 1);
+        const [A, B] = [a.HEAD.toLowerCase(), b.HEAD.toLowerCase()];
+        if (A > B) return 1;
+        else if (A === B) return (a.NUMBER * 1) - (b.NUMBER * 1);
         return -1;
     }).map((fileObj) => Object.values(fileObj).join(""));
 }
