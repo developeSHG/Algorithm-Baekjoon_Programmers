8d28be7bbd2b67e1b47243858d350a9b5eefdaf5
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-03 14:11:02 +0900

    [level 2] Title: 방문 길이, Time: 1.56 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,23 @@
+const solution = (dirs) => {
+    const [x, y, BOUNDARY] = [0, 1, 5];
+    const inArea = (command, pos) =>
+        (command === 'L' && pos[x] > -BOUNDARY)
+        || (command === 'R' && pos[x] < BOUNDARY)
+        || (command === 'D' && pos[y] > -BOUNDARY)
+        || (command === 'U' && pos[y] < BOUNDARY)
+
+    return [...dirs].reduce((acc, command) => {
+        if (!inArea(command, acc.pos)) return acc;
+
+        const prev = [...acc.pos];
+        (command === 'L') ? --acc.pos[x] : (command === 'R') ? ++acc.pos[x] : 0;
+        (command === 'D') ? --acc.pos[y] : (command === 'U') ? ++acc.pos[y] : 0;
+
+        if (!acc.history[[prev, acc.pos]] && !acc.history[[acc.pos, prev]]) {
+            acc.history[[prev, acc.pos]] = true;
+            acc.history[[acc.pos, prev]] = true;
+            ++acc.move;
+        }
+        return acc;
+    }, { pos: [0, 0], history: {}, move: 0 }).move;
+}
