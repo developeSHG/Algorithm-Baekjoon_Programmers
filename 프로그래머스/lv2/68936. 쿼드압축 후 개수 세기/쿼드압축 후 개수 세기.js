f95299fe69734cb7284ffcc9adf8de193e0e8775
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-08 18:23:39 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 9.56 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,22 +1,22 @@
-const quadTree = (arr, y, x, n, result) => {
-    for (let i = y; i < y + n; ++i) {
-        for (let j = x; j < x + n; ++j) {
-            if (arr[y][x] !== arr[i][j]) {
-                n = Math.floor(n / 2);
-                for (let q = 0; q < 4; ++q) {
-                    const [ny, nx] = [q >= 2 ? y + n : y, q % 2 ? x + n : x];
-                    result = quadTree(arr, ny, nx, n, result);
+const solution = (arr) => {
+    return (quadTree = (arr, y, x, n, result) => {
+        for (let i = y; i < y + n; ++i) {
+            for (let j = x; j < x + n; ++j) {
+                if (arr[y][x] !== arr[i][j]) {
+                    n = Math.floor(n / 2);
+                    for (let q = 0; q < 4; ++q) {
+                        const [ny, nx] = [
+                            q >= 2 ? y + n : y,
+                            q % 2 ? x + n : x,
+                        ];
+                        result = quadTree(arr, ny, nx, n, result);
+                    }
+                    return result;
                 }
-                return result;
             }
         }
-    }
-
-    return arr[y][x] === 0
-        ? [result[0] + 1, result[1]]
-        : [result[0], result[1] + 1];
+        return arr[y][x] === 0
+            ? [result[0] + 1, result[1]]
+            : [result[0], result[1] + 1];
+    })(arr, 0, 0, arr.length, [0, 0]);
 };
-
-function solution(arr) {
-    return quadTree(arr, 0, 0, arr.length, [0, 0]);
-}
