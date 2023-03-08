1740951435b076a81e22fb3ced504c39db3b6f1e
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-08 18:30:28 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 14.18 ms, Memory: 40.9 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -4,17 +4,12 @@ const solution = (arr) => {
             for (let j = x; j < x + n; ++j) {
                 if (arr[y][x] !== arr[i][j]) {
                     n = Math.floor(n / 2);
-                    for (let q = 0; q < 4; ++q) {
-                        const [ny, nx] = [
-                            q >= 2 ? y + n : y,
-                            q % 2 ? x + n : x,
-                        ];
-                        result = quadTree(arr, ny, nx, n, result);
-                    }
+                    for (let [ny, nx] of [[y, x], [y, x + n], [y + n, x], [y + n, x + n]]) result = quadTree(arr, ny, nx, n, result);
                     return result;
                 }
             }
         }
+
         return arr[y][x] === 0
             ? [result[0] + 1, result[1]]
             : [result[0], result[1] + 1];
