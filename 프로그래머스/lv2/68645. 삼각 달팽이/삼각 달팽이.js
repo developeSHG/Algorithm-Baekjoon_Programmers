cee2c6692fc004e89c51dfe29158a2e4887b3796
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-08 21:48:30 +0900

    [level 2] Title: 삼각 달팽이, Time: 137.97 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -3,19 +3,15 @@ function solution(n) {
     let [dir, cnt, col, row] = [0, 1, -1, 0];
 
     while (n) {
-        const colArrow = (dir === 0 ? 1 : dir === 2 ? -1 : 0);
-
         for (let i = 0; i < n; ++i) {
-            col += colArrow;
-            if (colArrow === 0) ++row;
-            else if (dir === 2) --row;
+            col += ((dir === 0) ? 1 : (dir === 2) ? -1 : 0);
+            row += ((dir === 1) ? 1 : (dir === 2) ? -1 : 0);
 
             if (!arr[col]) arr[col] = [];
             else if (arr[col].length < row) arr[col] = Array.from({ length: row + 1 }, (_, i) => arr[col][i] ? arr[col][i] : 0);
 
             arr[col][row] = cnt++;
         }
-        
         dir = ++dir % 3;
         --n;
     }
