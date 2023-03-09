6ca149c54f46d16f12cba97d17557d7d310d0940
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-09 17:56:34 +0900

    [level 2] Title: [3차] 방금그곡, Time: 4.01 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -2,10 +2,10 @@ function solution(m, musicinfos) {
     m = m.replaceAll('C#', 'c').replaceAll('D#', 'd').replaceAll('F#', 'f').replaceAll('G#', 'g').replaceAll('A#', 'a');
 
     return musicinfos.reduce((acc, data) => {
-        const [start, end, title] = data.split(',');
+        const [start, end, title, tempSheet] = data.split(',');
         const getMin = (time) => parseInt(time[0]) * 60 + parseInt(time[1]);
         const playTime = getMin(end.split(':')) - getMin(start.split(':'));
-        const sheet = data.split(',')[3].replaceAll('C#', 'c').replaceAll('D#', 'd').replaceAll('F#', 'f').replaceAll('G#', 'g').replaceAll('A#', 'a');
+        const sheet = tempSheet.replaceAll('C#', 'c').replaceAll('D#', 'd').replaceAll('F#', 'f').replaceAll('G#', 'g').replaceAll('A#', 'a');
         
         let newSheet = sheet.repeat(Math.floor(playTime / sheet.length));
         for (let i = 0; i < Math.floor(playTime % sheet.length); ++i)
