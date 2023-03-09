703ccefcb08d6b324a9954837db2ed487ec87667
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-09 13:59:09 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 13.66 ms, Memory: 45.9 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -4,6 +4,13 @@ function solution(queue1, queue2) {
     let [answer, sum1, sum2, idx1, idx2] = [0, sumFunc(queue1), sumFunc(queue2), 0, 0];
     const [half, limit] = [(sum1 + sum2) / 2, queue1.length * 3];
 
+    // shift 없이 idx를 사용하는 이유는 js는 queue를 지원하지 않기 때문에
+    // shift를 사용하면 시간복잡도(n)이 걸려서 시간초과가 날 수 있다.
+    
+    // limit는 최악의 케이스를 대비한 수.
+    // 최악의 케이스는 한 큐에서 하나를 제외하고, 다른 큐로 모두 보내는 경우이기 때문에
+    // 원래 길이 * 3을 넘어서 순회할 경우 return해서 탈출하도록 설정.
+    
     while (answer < limit) {
         if (sum1 === sum2)
             break;
