c71ae456d1f4cd460a244d4e92b9f4039489a191
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-09 18:05:02 +0900

    [level 2] Title: [3차] 방금그곡, Time: 2.08 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -2,13 +2,13 @@ function solution(m, musicinfos) {
     m = m.replaceAll('C#', 'c').replaceAll('D#', 'd').replaceAll('F#', 'f').replaceAll('G#', 'g').replaceAll('A#', 'a');
 
     return musicinfos.reduce((acc, data) => {
-        const [start, end, title, tempSheet] = data.split(',');
+        const [start, end, TITLE, tempSheet] = data.split(',');
         const getMin = (time) => parseInt(time[0]) * 60 + parseInt(time[1]);
-        const playTime = getMin(end.split(':')) - getMin(start.split(':'));
+        const TIME = getMin(end.split(':')) - getMin(start.split(':'));
         const sheet = tempSheet.replaceAll('C#', 'c').replaceAll('D#', 'd').replaceAll('F#', 'f').replaceAll('G#', 'g').replaceAll('A#', 'a');
-        const newSheet = sheet.repeat(Math.floor(playTime / sheet.length)) + sheet.substr(0, Math.floor(playTime % sheet.length));
+        const newSheet = sheet.repeat(Math.floor(TIME / sheet.length)) + sheet.substr(0, Math.floor(TIME % sheet.length));
 
-        if (newSheet.includes(m) && playTime > acc.TIME) acc = { TITLE: title, TIME: playTime };
+        if (newSheet.includes(m) && TIME > acc.TIME) acc = { TITLE, TIME };
         return acc;
     }, { TITLE: "(None)", TIME: 0 }).TITLE;
 }
