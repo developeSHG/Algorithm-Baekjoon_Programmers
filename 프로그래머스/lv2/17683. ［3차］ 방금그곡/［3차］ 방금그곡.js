45f8416ecf4f3ed7c830bbf7480afb62298b3ddc
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-09 17:54:10 +0900

    [level 2] Title: [3차] 방금그곡, Time: 3.76 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.js
@@ -6,10 +6,8 @@ function solution(m, musicinfos) {
         const getMin = (time) => parseInt(time[0]) * 60 + parseInt(time[1]);
         const playTime = getMin(end.split(':')) - getMin(start.split(':'));
         const sheet = data.split(',')[3].replaceAll('C#', 'c').replaceAll('D#', 'd').replaceAll('F#', 'f').replaceAll('G#', 'g').replaceAll('A#', 'a');
-        let newSheet = '';
         
-        for (let i = 0; i < Math.floor(playTime / sheet.length); ++i)
-            newSheet += sheet;
+        let newSheet = sheet.repeat(Math.floor(playTime / sheet.length));
         for (let i = 0; i < Math.floor(playTime % sheet.length); ++i)
             newSheet += sheet[i];
 
