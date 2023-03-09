f3e047b35dca7eee229862b76a875b356ace18c1
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-09 13:54:37 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 15.99 ms, Memory: 45.9 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -4,28 +4,19 @@ function solution(queue1, queue2) {
     let [answer, sum1, sum2, idx1, idx2] = [0, sumFunc(queue1), sumFunc(queue2), 0, 0];
     const [half, limit] = [(sum1 + sum2) / 2, queue1.length * 3];
 
-    while (true) {
-        if (answer >= limit) return -1;
-        
+    while (answer < limit) {
         if (sum1 === sum2)
             break;
-        const data = (sum1 > sum2) ? queue1[idx1++] : queue2[idx2++];
-
-        if (data > half)
-            return -1;
 
         if (sum1 > half) {
-            queue2.push(data);
-            sum1 -= data;
-            sum2 += data;
+            const data = queue1[idx1++];
+            sum1 -= data, sum2 += data, queue2.push(data);
         } else {
-            queue1.push(data);
-            sum1 += data;
-            sum2 -= data;
+            const data = queue2[idx2++];
+            sum1 += data, sum2 -= data, queue1.push(data);
         }
-
         ++answer;
     }
 
-    return answer;
+    return (answer < limit) ? answer : -1;
 }
