2a5e30c85c8ab1833c70dd2a70249d7dba5aaa82
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-16 19:26:56 +0900

    [level 2] Title: 배달, Time: 1.35 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.js
+++ 프로그래머스/lv2/12978. 배달/배달.js
@@ -1,22 +1,22 @@
 function solution(N, road, K) {
-    const graph = road.reduce((obj, [p1, p2, t]) => {
+    const graph = road.reduce((obj, [p1, p2, t]) => { 
         (obj[p1] = obj[p1] || {}), (obj[p2] = obj[p2] || {});
         obj[p1][p2] = obj[p2][p1] = Math.min(obj[p1][p2] ? obj[p1][p2] : Infinity, t);
         return obj;
-    }, {});
+    }, {}); // 무방향 그래프
 
-    const stack = [{ dest: 1, cost: 0 }],
-        distance = new Array(N + 1).fill(Infinity);
+    const stack = [{ dest: 1, cost: 0 }], // 첫 노드로 가는 정보를 삽입
+        distance = new Array(N + 1).fill(Infinity); // distance는 각 노드의 최단 경로를 저장
 
-    while (stack.length) {
-        const { dest: cur, cost: cost } = stack.shift();
+    while (stack.length) { // dfs
+        const { dest: cur, cost: cost } = stack.pop();
 
-        if (distance[cur] > cost) distance[cur] = cost;
+        if (distance[cur] > cost) distance[cur] = cost; // 최단 경로로 갱신
 
         for (const dest in graph[cur]) {
-            if (distance[dest] < cost + graph[cur][dest]) continue;
+            if (distance[dest] < cost + graph[cur][dest]) continue; // dest 좌표의 경로 값보다, 현재 좌표에서 dest 좌표로 가는 경로가 크면 굳이 방문을 안함.
             stack.push({ dest: dest, cost: cost + graph[cur][dest] });
         }
     }
-    return distance.filter((e) => e !== Infinity && e <= K).length;
+    return distance.filter((e) => e !== Infinity && e <= K).length; // K보다 낮은 최단 경로의 갯수만 추출
 }
