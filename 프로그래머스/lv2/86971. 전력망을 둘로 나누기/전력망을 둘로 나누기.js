7c689110c37a6fabd8dc6f161819a80d98e1b9e9
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-17 14:48:47 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.14 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -0,0 +1,44 @@
+function solution(n, wires) {
+    const graph = wires.reduce((acc, [a, b]) => {
+            acc[a].push(b), acc[b].push(a);
+            return acc;
+        }, Array.from(Array(n + 1), () => []));
+
+    const visitedNone = visitedDivision = new Array(n + 1).fill(false);
+
+    const dfs = (pivot, visited, vistedDuplication) => {
+        const queue = [pivot];
+        let cnt = 0;
+
+        while (queue.length) {
+            const dest = queue.shift();
+            if (vistedDuplication[dest]) return Infinity;
+
+            (visited[dest] = true), ++cnt;
+            for (const to of graph[dest]) {
+                if (visited[to]) continue;
+                queue.push(to);
+            }
+        }
+        return cnt;
+    };
+
+    let result = Infinity;
+    graph.forEach((form, idx) => {
+        for (const division of form) {
+            visitedNone[idx] = visitedDivision[idx] = true;
+            const divisionCnt = dfs(division, visitedDivision, visitedNone);
+
+            let nodeCnt = 1;
+            for (const node of form.filter((e) => e !== division))
+                nodeCnt += dfs(node, visitedNone, visitedDivision);
+
+            result = Math.min(result, Math.abs(divisionCnt - nodeCnt));
+
+            visitedDivision.fill(false);
+            visitedNone.fill(false);
+        }
+    });
+
+    return result;
+}
