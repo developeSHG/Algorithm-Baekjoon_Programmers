aa7379f429d8461819359d337864afd9f491d045
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-20 16:34:03 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 54.56 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
+++ 프로그래머스/lv2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -5,13 +5,11 @@ function solution(board) {
 
         rows.forEach((column, j) => {
             if (!j) return;
-
             if (column) {
                 board[i][j] = Math.min(board[i - 1][j - 1], board[i - 1][j], board[i][j - 1]) + 1;
                 acc = Math.max(acc, board[i][j]);
             }
         });
-
         return i !== board.length - 1 ? acc : acc * acc;
     }, 0);
 }
