f6e850a64de0457821b044edeefe4c7359ca2817
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-26 20:17:49 +0900

    [level 2] Title: 문자열 압축, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -1,4 +1,24 @@
 function solution(s) {
-    var answer = 0;
+    let answer = s.length;
+
+    for (let i = 1; i <= Math.floor(s.length / 2); ++i) {
+        let [str, word, idx, cnt] = ["", "", 0, 1];
+
+        while (idx < s.length) {
+            const temp = s.substr(idx, i);
+            idx += i;
+
+            if (temp === word) ++cnt;
+            else {
+                str += `${(cnt > 1 ? cnt : "")}${word}`;
+                (cnt = 1), (word = temp);
+
+                if (idx >= s.length) str += temp;
+            }
+        }
+
+        if (cnt > 1) str += `${cnt}${word}`;
+        answer = Math.min(str.length, answer);
+    }
     return answer;
 }
