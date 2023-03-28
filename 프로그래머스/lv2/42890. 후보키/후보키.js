b8ea7d0edcbd53118561d8387ceb865990d24b5e
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-03-28 20:24:23 +0900

    [level 2] Title: 후보키, Time: 0.89 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42890. 후보키/후보키.js
@@ -0,0 +1,63 @@
+// 조합
+const getCombination = (elements, pick) => {
+    if (pick === 1) return elements.map((elem) => [elem]);
+
+    const combinations = [];
+    elements.forEach((element, index) => {
+        const smallerCombinations = getCombination(
+            elements.slice(index + 1),
+            pick - 1
+        );
+        smallerCombinations.forEach((combination) =>
+            combinations.push([element].concat(combination))
+        );
+    });
+
+    return combinations;
+};
+
+// 유일성 체크
+const uniqueness = (relation, elements, row) => {
+    const arr = new Array(row).fill("");
+    elements.forEach((attr) => {
+        for (let tuple = 0; tuple < row; ++tuple)
+            arr[tuple] += relation[tuple][attr];
+    });
+
+    return !arr.some((x) => arr.indexOf(x) !== arr.lastIndexOf(x)); // 중복체크
+};
+
+// 최소성 체크
+const minimality = (answer, elements) => {
+    return answer.length
+        ? answer.every(
+              (arr) =>
+                  arr.filter((a) => elements.some((b) => a === b)).length !==
+                  arr.length
+          )
+        : true;
+};
+
+function solution(relation) {
+    const answer = [];
+    const [attribute, row, column] = [
+        Array.from({ length: relation[0].length }, (_, idx) => idx),
+        relation.length,
+        relation[0].length,
+    ];
+
+    for (let i = 1; i <= column; ++i) {
+        const combinations = getCombination(attribute, i);
+
+        combinations.forEach((elements) => {
+            if (
+                uniqueness(relation, elements, row) &&
+                minimality(answer, elements)
+            ) {
+                answer.push(elements);
+            }
+        });
+    }
+
+    return answer.length;
+}
