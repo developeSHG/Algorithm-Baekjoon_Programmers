f2d90318547a29dc25726aeffe9e93b256d88a42
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-04-01 03:05:37 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.js
@@ -0,0 +1,17 @@
+function solution(storey) {
+    storey = storey.toString();
+    let result = Infinity;
+
+    (dfs = (pivot, acc, add) => {
+        if (pivot < 0) {
+            result = Math.min(result, acc + add);
+            return;
+        }
+
+        const num = parseInt(storey[pivot]) + add;
+        dfs(pivot - 1, acc + num, 0);
+        dfs(pivot - 1, acc + 10 - num, 1);
+    })(storey.length - 1, 0, 0);
+
+    return result;
+}
