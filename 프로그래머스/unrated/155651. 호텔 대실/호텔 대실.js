dbdfce071dfc02ee98a19ecfbdb5b6d47a5f5389
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-04-03 12:39:12 +0900

    [unrated] Title: 호텔 대실, Time: 33.52 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.js
@@ -1,37 +1,19 @@
 function solution(book_time) {
-    let rooms = 0;
-    let [livinig, empty] = [[], []];
+    let [rooms, livinig, empty] = [0, [], []];
     const GetTime = (n) => Number(n[0]) * 60 + Number(n[1]);
 
-    book_time = book_time.map(([entrance, leave]) => [
-        GetTime(entrance.split(":")),
-        GetTime(leave.split(":")),
-    ]);
-    book_time;
-
-    book_time = book_time.sort((a, b) => a[0] - b[0]);
-    book_time;
+    book_time = book_time.map(([entrance, leave]) => [GetTime(entrance.split(":")), GetTime(leave.split(":"))]).sort((a, b) => a[0] - b[0]);
 
     book_time.forEach(([entrance, leave]) => {
-        if (rooms === 0) {
-            livinig.push([rooms++, leave]);
-            return;
-        }
-
         livinig = livinig.filter(([num, livingLeave]) => {
-            return livingLeave + 10 > entrance
-                ? true
-                : (function () {
-                      empty.push(num);
-                      return false;
-                  })();
+            return livingLeave + 10 > entrance 
+            ? true : (function () {
+                empty.push(num);
+                return false;
+            })();
         });
 
-        if (!empty.length) {
-            livinig.push([++rooms, leave]);
-        } else livinig.push([empty.pop(), leave]);
-
+        !empty.length ? livinig.push([++rooms, leave]) : livinig.push([empty.pop(), leave]);
     });
-
     return rooms;
 }
