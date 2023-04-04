fe99979d6523751ff643efa8f4d8808ffaff0ac7
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-04-04 14:56:19 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,13 +1,10 @@
-const divide = (a, b) => {
-    let pivot = a[0];
+const divide = (cur, target) => {
+    let pivot = cur[0];
     while (pivot) {
-        if (a.every((num) => !(num % pivot)) && b.every((num) => num % pivot)) return pivot;
+        if (cur.every((num) => !(num % pivot)) && !target.some((num) => !(num % pivot))) return pivot;
         --pivot;
     }
-
-    return pivot;
+    return 0;
 };
 
-function solution(arrayA, arrayB) {
-    return Math.max(divide(arrayA, arrayB), divide(arrayB, arrayA));
-}
+function solution(arrayA, arrayB) { return Math.max(divide(arrayA, arrayB), divide(arrayB, arrayA)) }
