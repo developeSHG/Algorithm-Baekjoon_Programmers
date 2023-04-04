10832b7e48afbb20a0e416a253f6d9f98a2c9439
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-04-04 20:12:46 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.js
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.js
@@ -2,7 +2,6 @@ function solution(cards) {
     const [box, group] = [new Array(cards.length).fill(false), []];
     let i = 0;
 
-    // while (group.some((v) => !v) && box.some((v) => !v)) {
     while (box.some((v) => !v)) {
         let num = box.indexOf(false);
         while (!box[num] && num < box.length) {
