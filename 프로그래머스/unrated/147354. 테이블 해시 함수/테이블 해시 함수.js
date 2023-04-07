b12a426622f8e1187326fbd4060ffd94aadb9a3a
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-04-07 12:08:14 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -1,8 +1,5 @@
 function solution(data, col, row_begin, row_end) {
-    data = data.sort((a, b) => {
-        const res = a[col - 1] - b[col - 1];
-        return res === 0 ? b[0] - a[0] : res;
-    });
+    data = data.sort((a, b) => a[col - 1] - b[col - 1] || b[0] - a[0]);
 
     const answer = [];
     for (let idx = row_begin; idx <= row_end; ++idx)
