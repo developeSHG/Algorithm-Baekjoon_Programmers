1b2e2f5b227d657b2e3ad034c384c953f18d0b5f
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-05-16 12:43:40 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 498.24 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    int count = 0, erase = 0;
+
+    while (s != "1")
+    {
+        while (find(s.begin(), s.end(), '0') != s.end())
+        {
+            s.erase(find(s.begin(), s.end(), '0'));
+            ++erase;
+        }
+
+        s = bitset<100>(s.length()).to_string();
+        s = s.substr(s.find('1'));
+        ++count;
+    }
+
+    return vector<int>{ count, erase };
+}
