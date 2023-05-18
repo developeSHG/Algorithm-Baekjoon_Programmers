9f92ff670c269db3349dc7b52598fc73b39cfbe3
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-05-18 14:49:45 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 1;
+    int answer = 1; // 자기자신 + 1
 
+    // 두 개의 반복문을 이용해 연속된 숫자로 n이 되면 ++answer로 증가.
     for (int i = 1; i <= (n / 2); ++i)
     {
         int sum = i;
@@ -13,7 +14,7 @@ int solution(int n) {
         {
             sum += j;
             if (sum == n) ++answer;
-            else if (sum > n) break;
+            else if (sum > n) break; // 연속된 숫자의 합이 n보다 커질경우, 반복문 종료
         }
     }
     return answer;
