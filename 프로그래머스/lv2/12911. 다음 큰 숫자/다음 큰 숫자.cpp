850b6674bfb2f345cef95807de98b6675013fed8
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-05-19 12:26:51 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.75 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int bit1_number(int n)
+{
+    int cnt = 0;
+
+    while (n)
+    {
+        int remain = (n % 2);
+        n /= 2;
+
+        cnt += (remain == 1) ? 1 : 0;
+    }
+
+    return cnt;
+}
+
+int solution(int n) {
+    int result = bit1_number(n);
+    while (result != bit1_number(n + 1)) ++n;
+
+    return ++n;
+}
