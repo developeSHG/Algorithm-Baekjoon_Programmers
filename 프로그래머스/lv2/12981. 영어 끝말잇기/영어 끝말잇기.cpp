ea053cb1873ce3d520ccf1f3758bd688d3486233
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-05-23 16:33:17 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.05 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -8,19 +8,17 @@ using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
     set<string> s;
-    int people{ 0 }, cnt{ 1 };
-    char lastWord = words[0][0];
+    int cnt{ 0 };
     
     auto res = any_of(words.begin(), words.end(), [&](const auto& str)
     {
-        if (++people > n) people = 1, ++cnt;
-
+        const auto& lastWord = cnt ? words[cnt - 1].back() : words[0].front();
         if (s.find(str) != s.end() || lastWord != str.front())
             return true;
-        
-        s.insert(str), lastWord = str.back();
+ 
+        s.insert(str), ++cnt;
         return false;
     });
     
-    return res ? vector<int>{ people, cnt } : vector<int>{ 0, 0 };
+    return res ? vector<int>{ cnt % n + 1, cnt / n + 1 } : vector<int>{ 0, 0 };
 }
