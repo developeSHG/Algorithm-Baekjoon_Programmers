4e8b5151b182e0559af9b5dd75a1a0a73127d2df
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-05-25 11:07:34 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    vector<int> answer;
+    /*
+    가로 a, 세로 b일 때
+    테두리의 타일 개수: 2 * a + (b-2) * 2 = 2 *a + 2 * b - 4 = brown
+    가운데 노란색의 타일 개수 = a * b - brown = yellow
+    i * b - (2*i + 2*b -4) = yellow
+    (i - 2) * b = yellow + 2*i -4
+
+    정리하면, 
+    a * b - brown = yellow
+    a * b = brown + yellow
+    */
+    for(int i=1; i<=5000; i++){//가로의 길이
+        for(int j=1; j<=i; j++){
+            if(2 * i + 2 * j - 4 == brown){
+                if(i * j - brown == yellow){
+                    answer.push_back(i);
+                    answer.push_back(j);
+                    return answer;
+                }
+            }
+        }
+    }
+    return answer;
+}
