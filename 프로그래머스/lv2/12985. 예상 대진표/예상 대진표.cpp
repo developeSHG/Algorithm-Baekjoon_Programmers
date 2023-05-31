b2fdb5240dd9d64a3414652a00f8a58f197806e7
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-05-31 11:43:27 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <stack>
+#include <set>
+
+using namespace std;
+
+class Command
+{
+public:
+    Command(const int& a, const int& b)
+        : _a(min(a, b)), _b(max(a, b))
+    {
+       
+    }
+    const bool operator() ()
+    {
+        return (_a % 2) && !(_b % 2) && (_b - _a == 1);
+    }
+
+public:
+    void vs()
+    {
+        _a = ceil(_a / 2.f);
+        _b = ceil(_b / 2.f);
+    }
+
+private:
+    int _a;
+    int _b;
+};
+
+int solution(int n, int a, int b)
+{
+    int answer = 1;
+
+    Command cmd(a, b);
+    while (!cmd())
+    {
+        cmd.vs();
+        ++answer;
+    }
+
+    return answer;
+}
