3d6042085e1ba05041ee9ff6fe911521f0b84a92
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-06-02 12:44:05 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int n) {
+    long long arr[2001] = { 1, 1 };
+    
+    for (int i = 2; i <= n; i++)
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+    
+    return arr[n];
+}
