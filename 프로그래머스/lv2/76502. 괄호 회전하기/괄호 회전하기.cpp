c3c138cea2f5056654df49f5275335c079b2651d
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-06-10 23:32:03 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.cpp
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -10,11 +10,11 @@ int solution(string s) {
     for (auto iter = s.begin(); iter != s.end(); ++iter)
     {
         string temp = s;
-        stack<char> st;
+        stack<char> st; // 후입선출로 괄호가 올바른지 체크
 
-        int start = iter - s.begin();
-        temp.erase(0, start);
-        temp += s.substr(0, start);
+        int start = iter - s.begin(); // iter에서 begin과의 차이가 회전시킬 문자의 수
+        temp.erase(0, start); // 앞에서부터 삭제
+        temp += s.substr(0, start); // 맨 뒤에 붙여줌
 
         for (const auto word : temp)
         {
@@ -24,11 +24,12 @@ int solution(string s) {
                 || (st.top() == '(' && word == ')')
                 || (st.top() == '{' && word == '}'))
                 st.pop();
-            else if (word == ']' || word == ')' || word == '}') break;
+            else if (word == ']' || word == ')' || word == '}') break; // 닫는 괄호일 경우, 뒤에 더이상 체크할 필요가 없어서 break;
             else st.push(word);
+
         };
 
-        if (st.empty())
+        if (st.empty()) // 비어있으면 올바르다고 판단
             ++answer;
     }
 
