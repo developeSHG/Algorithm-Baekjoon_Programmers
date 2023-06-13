47496bdcf2a40e1cd86f2237cd7096cabc549dc6
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-06-13 11:25:38 +0900

    [level 2] Title: n^2 배열 자르기, Time: 22.99 ms, Memory: 20.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+
+    // 굳이 2차원 배열을 만들어서 모든 값을 초기화할 필요는 없다.
+    // left와 right 까지의 필요한 공간만을 n으로 행열을 계산해서 도출할 수 있다.
+    for (long i = left; i <= right; ++i)
+    {
+        // +1을 다 해주는 이유는 프로그래밍상에선 0부터 시작이니 +1
+        auto a = i / n + 1; // 행
+        auto b = i % n + 1; // 열
+
+        // a > b : 해당 행과 열이 가져야 하는 최소값보다 낮다는 것이니 행을 대입
+        answer.push_back((a > b) ? a : b); 
+    }
+
+    return answer;
+}
