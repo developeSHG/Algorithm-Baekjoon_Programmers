69b5d064c2bddbb5bb93e468f200f967d9338d1f
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-06-16 11:19:47 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.cpp
@@ -8,13 +8,10 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
 
     int day = 0;
-
     for (auto iter = progresses.begin(); iter != progresses.end(); ++iter)
     {
         int speed = speeds[iter - progresses.begin()];
         int time = ceil((100 - *iter) / static_cast<float>(speed));
-
-        if (day == 0) day = time;
 
         if (answer.empty() || day < time)
         {
