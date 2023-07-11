4fbb3b3af1571df4eda5623f1d67e421bb102fec
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-07-11 23:25:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.24 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -0,0 +1,58 @@
+#include<vector>
+#include<queue>
+using namespace std;
+
+/*void dfs(int x, int y, int acc, vector<vector<int>>&maps)
+{
+    maps[x][y] = acc;
+
+    for (size_t i = 0; i < 4; ++i)
+    {
+        const auto nx = x + dir[i][0],
+            ny = y + dir[i][1];
+
+        if (nx < 0 || nx >= maps.size() || ny < 0 || ny >= maps[0].size() || maps[nx][ny] == 0)
+            continue;
+
+        if (maps[nx][ny] == 1 || maps[nx][ny] > acc + 1)
+            dfs(nx, ny, acc + 1, maps);
+    }
+}*/
+
+using p = pair<int, int>;
+
+int solution(vector<vector<int>> maps)
+{
+    int n = maps.size(), m = maps[0].size();
+    int dist = 1;
+    const vector<vector<int>> dir = { {1, 0}, {0, -1}, {0, 1}, {-1, 0} };
+    queue<p> q;
+
+    q.push(make_pair(0, 0));
+
+    while (!q.empty())
+    {
+        const int x = q.front().first, y = q.front().second;
+        q.pop();
+
+        for (size_t i = 0; i < dir.size(); ++i)
+        {
+            const auto nx = x + dir[i][0],
+                        ny = y + dir[i][1];
+
+            if (nx < 0 || nx >= maps.size() || ny < 0 || ny >= maps[0].size() || maps[nx][ny] == 0)
+                continue;
+
+            if (maps[nx][ny] == 1 || maps[nx][ny] > maps[x][y] + 1)
+            {
+                q.push(make_pair(nx, ny));
+                maps[nx][ny] = maps[x][y] + 1;
+            }
+
+            if (maps[n - 1][m - 1] > 1)
+                break;
+        }
+    }
+    
+    return maps[n - 1][m - 1] == 1 ? -1 : maps[n - 1][m - 1];
+}
