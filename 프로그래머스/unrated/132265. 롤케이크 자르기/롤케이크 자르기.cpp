efa6b368114dd3c1e071f0a5da463b4a93ff308e
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-07-21 16:20:18 +0900

    [unrated] Title: 롤케이크 자르기, Time: 156.86 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int solution(vector<int> topping) {
+    map<int, int> a, b;
+    std::for_each(topping.begin(), topping.end(), [&](const auto& t) { ++b[t]; });
+    return std::accumulate(topping.begin(), topping.end(), 0, [&](auto acc, const auto& t) {
+        ++a[t], --b[t];
+        if (b[t] == 0) b.erase(t);
+        return (a.size() == b.size()) ? acc + 1 : acc;
+    });
+}
