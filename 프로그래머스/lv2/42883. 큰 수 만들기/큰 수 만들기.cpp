98370c48d4252f379d0cce3c24a42d139447fefb
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-07-27 12:29:34 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -11,9 +11,11 @@ string solution(string number, int k) {
 
     while (answer.length() != cnt)
     {
+        // end에서 구해야 할 남은 개수만큼 뺀 영역까지 최대 값 도출
         auto temp = max_element(number.begin() + start, number.end() - cnt + answer.length() + 1);
-        answer += *temp;
-        start = temp - number.begin() + 1;
+
+        // start를 새롭게 갱신
+        answer += *temp, start = temp - number.begin() + 1;
     }
     
     return answer;
