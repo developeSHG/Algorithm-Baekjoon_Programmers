1a634ae5837367b8c53ff4ead238b071ebc899ee
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-07-28 12:25:14 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.01 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.cpp
+++ 프로그래머스/lv2/12899. 124 나라의 숫자/124 나라의 숫자.cpp
@@ -5,16 +5,13 @@ using namespace std;
 
 string solution(int n) {
     string answer = "", str = "412";
-    bool three = (n % 3 == 0);
-    n -= three ? 1 : 0;
 
     while (n)
     {
         answer = str[n % 3] + answer;
-        if (n % 3 == 0) --n;
+        if (n % 3 == 0) --n; // 3인 경우는 4로 표현하기 때문에 -1
         n = n / 3;
     }
 
-    if (three) answer.back() = '4';
     return answer;
 }
