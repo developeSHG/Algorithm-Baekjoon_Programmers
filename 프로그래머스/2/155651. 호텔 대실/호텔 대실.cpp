a2c2d6365d4a0f0ca4a8286f339b2fd0ffb935ae
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2023-10-11 11:33:51 +0900

    [level 2] Title: 호텔 대실, Time: 1.20 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int solution(vector<vector<string>> book_time) {
+    vector<int> rooms;
+
+    std::sort(book_time.begin(), book_time.end(), [](const auto& v1, const auto& v2) {
+        return v1.front() < v2.front();
+    });
+
+    std::for_each(book_time.begin(), book_time.end(), [&](const auto& v) {
+        stringstream ss1(v.front());
+        stringstream ss2(v.back());
+        string buffer;
+        vector<int> temp;
+
+        while (getline(ss1, buffer, ':')) temp.emplace_back(stoi(buffer));
+        int s_time = temp[0] * 60 + temp[1];
+        
+        temp.clear();
+        while (getline(ss2, buffer, ':')) temp.emplace_back(stoi(buffer));
+        int e_time = temp[0] * 60 + temp[1];
+        
+        for (size_t i = 0; i < rooms.size(); ++i)
+        {
+            if (rooms[i] <= s_time)
+            {
+                rooms[i] = e_time + 10;
+                return;
+            }
+        }
+
+        rooms.emplace_back(e_time + 10);
+    });
+
+    return rooms.size();
+}
