d36b48bbcdb81deb682dc326b13fcd8f0b755e68
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2024-10-11 20:11:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -1,29 +1,19 @@
 #include <string>
 #include <vector>
+#include <sstream>
 #include <algorithm>
-#include <iostream>
+
 using namespace std;
 
-string solution(string s) 
-{
-	string answer = "";
-	string tmp;
-	vector<int> v1;
-    
-	for (int i = 0; i < s.size(); i++)
-	{
-		if(s[i] == ' ' )
-		{
-			v1.push_back(atoi(tmp.c_str()));
-			tmp.clear();
-		}
-		else
-		{
-			tmp += s[i];
-		}
-	}
-	v1.push_back(atoi(tmp.c_str()));
-	sort(v1.begin(), v1.end(), greater<int>());
-	answer += to_string(v1.back()) + " " +to_string(v1.front());
-	return answer;
+string solution(string s) {
+    string answer = "";
+
+    istringstream ss(s);
+    string subs1;
+    vector<int> v;
+
+    while (getline(ss, subs1, ' ')) v.push_back(stoi(subs1));
+
+    answer = to_string(*min_element(v.begin(), v.end())) + " " + to_string(*max_element(v.begin(), v.end()));
+    return answer;
 }
