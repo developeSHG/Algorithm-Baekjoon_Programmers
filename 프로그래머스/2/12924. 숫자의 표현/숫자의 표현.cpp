5cd27cb4c75cf6ed5b3dfd4a8e8e3a56808c96b5
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2024-10-14 00:17:14 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int lVal = 0, rVal = 0, sum = 0;
+    
+    while (true)
+    {
+        (sum <= n) ? ++rVal : ++lVal;
+        sum += (sum <= n) ? rVal : -lVal;
+        if (sum == n) 
+            ++answer;
+        
+        if (lVal == n) break;
+    }
+
+    return answer;
+}
