dd82378eb19cc42d3648c902159bdadd4fcc36ac
Song Ho Kyung <79896868+developeSHG@users.noreply.github.com>
2024-12-18 23:44:08 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -1,56 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 int solution(int storey) {
+    // 24.12.18
     int answer = 0;
-    vector<int> v;
-
-    auto str = to_string(storey);
-    for (const auto& element : str)
-        v.push_back(element - '0');
-
-    int order = v.size() - 1;
-    while (order)
+    while(storey)
     {
-        int type = 0;
-
-        vector<int> v_case =
-        {
-            v[order] + v[order - 1],
-            v[order] + 10 - (v[order - 1] + 1),
-            10 - v[order] + v[order - 1] + 1,
-            10 - v[order] + 10 - (v[order - 1] + 1)
-        };
-
-        const auto& iter = min_element(v_case.begin(), v_case.end());
-        if (*iter == v_case[0] || *iter == v_case[1]) answer += v[order];
-        else answer += 10 - v[order];
-               
-        if (*iter == v_case[2] || *iter == v_case[3])
-        {
-            int temp = order - 1;
-            do
-            {
-                ++v[temp];
-                if (v[temp] >= 10)
-                {
-                    v[temp] = 0;
-                    --temp;
-                    
-                    if (temp < 0) ++answer;
-                }
-                else break;
-            } while (temp >= 0);
-        }
-
-        --order;
+        int n = storey % 10;
+        storey /= 10;
+        
+        bool isUp = false;
+        if (n == 5 && storey % 10 >= 5) isUp = true;
+        else isUp = (n > 5); 
+            
+        answer += isUp ? (10 - n) : n; 
+        storey += isUp ? 1 : 0;
     }
-
-    answer += min(10 - v[0] + 1, v[0]);
-    
     return answer;
 }
